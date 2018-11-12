d57d1615e4ba961a2829a7bb6dd9beb08bf7248e
ku-wolf <kevinuwolf@gmail.com>
2018-11-12 13:40:45 -0500

    update readme, fix logger, use env in interpreter directive

--- kutils4p/logger.py
+++ kutils4p/logger.py
@@ -10,9 +10,8 @@ class Logger(object):
         """Init Logger set whether active by default."""
         self.log_level = log_level
 
-    def print(self, *args, **kwargs):
+    def print(self, level, *args, **kwargs):
         """Log string if logger active."""
-        level = kwargs["level"]
         if self.log_level >= level:
             print(*args, **kwargs)
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Setup module for kutils4p pkg."""
 from setuptools import setup, find_packages
 import os
